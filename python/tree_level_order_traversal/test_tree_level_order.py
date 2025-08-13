722b5f4475473c422e06ef8b230a83beb3f63943
Cicero Caze <cicerocaze@gmail.com>
2025-08-13 11:47:26 +0000

    fix(naming): use of snake case pattern

--- python/tree_level_order_traversal/test_tree_level_order.py
+++ python/tree_level_order_traversal/test_tree_level_order.py
@@ -1,10 +1,10 @@
 import unittest
-from tree_level_order import Node, levelOrder
+from tree_level_order import Node, level_order
 
 class TestLevelOrder(unittest.TestCase):
     def test_single_node(self):
         root = Node(1)
-        self.assertEqual(levelOrder(root), [1])
+        self.assertEqual(level_order(root), [1])
 
     def test_complete_tree(self):
         root = Node(1)
@@ -12,13 +12,13 @@ class TestLevelOrder(unittest.TestCase):
         root.right = Node(3)
         root.left.left = Node(4)
         root.left.right = Node(5)
-        self.assertEqual(levelOrder(root), [1, 2, 3, 4, 5])
+        self.assertEqual(level_order(root), [1, 2, 3, 4, 5])
 
     def test_left_skewed(self):
         root = Node(1)
         root.left = Node(2)
         root.left.left = Node(3)
-        self.assertEqual(levelOrder(root), [1, 2, 3])
+        self.assertEqual(level_order(root), [1, 2, 3])
 
 if __name__ == '__main__':
     unittest.main()    
